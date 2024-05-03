8844733326a1ce04444a2978aa824d364bae45ea
Md. Atikur Zaman <arzaman@ael-bd.com>
2024-05-04 02:28:00 +0600

    'initial'

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from "express";
+const port = 8000;
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("HELLO FROM ESS + TS!!!!");
+});
+
+app.get("/hi", (req: Request, res: Response) => {
+  res.send("BYEEE!!");
+});
+
+app.listen(port, () => {
+  console.log(`Now listening on port ${port}`);
+});
